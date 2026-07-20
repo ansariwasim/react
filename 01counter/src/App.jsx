c5c061f03e47d9d21e26249b79a573cc756cb944
Wasim Ansari <oasimansari0786@gmail.com>
2026-07-20 17:35:40 +0545

    counterproject

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,40 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  let[counter, setCounter] = useState(1)
+
+
+  const addValue = () =>{
+
+    if(counter < 20){
+    setCounter(counter + 1);
+
+    }
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter -1)
+    }
+
+  }
+  return (
+    <>
+
+ <h1>Hello wasim</h1>
+ <h2>Counter value: {counter}</h2>
+
+ <button onClick={addValue}>Add value  {counter}</button>
+ <br />
+ <button onClick={removeValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
